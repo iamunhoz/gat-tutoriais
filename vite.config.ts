89f22ea37f0d74f5c6f55b353cfc9ada8723472d
Ivã Munhoz <dev.iamunhoz@gmail.com>
2024-01-19 13:45:51 -0300

    fix build error 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import mdPlugin, { Mode }  from 'vite-plugin-markdown';
-import injectHTML from 'vite-plugin-html-inject';
+// import mdPlugin, { Mode }  from 'vite-plugin-markdown';
+// import injectHTML from 'vite-plugin-html-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    injectHTML(),
-    mdPlugin({
+    //injectHTML(),
+    //mdPlugin({
       /* mode: [Mode.MARKDOWN] */
-    }),
+    //}),
 /*     {
       name: 'html-loader',
       enforce: 'pre',
